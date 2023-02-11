7ca7d3bd7669b74b73020d629bbe0554b699e221
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-11 07:57:41 -0300

    asa

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -1,8 +1,8 @@
-import Joi from "joi";
+import joi from "joi";
 
-export const customerSchemma = Joi.object({
-  name: Joi.string().required(),
-  cpf: Joi.string().pattern(/^\d+$/).min(11).max(11).required(),
-  phone: Joi.string().pattern(/^\d+$/).max(11).min(10).required(),
-  birthday: Joi.date().required(),
+export const customerSchemma = joi.object({
+  name: joi.string().min(2).required(),
+  phone: joi.string().pattern(/^\d+$/).max(11).min(10).required(),
+  cpf: joi.string().pattern(/^\d+$/).min(11).max(11).required(),
+  birthday: joi.required(),
 });
