58607af12661f04f896a8e7dce296cae5c728af5
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-11 07:32:53 -0300

    aaa

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const customerSchemma = joi.object({
   name: joi.string().min(2).required(),
   phone: joi.string().max(11).required(),
-  cpf: joi.number().min(11).max(11).required(),
+  cpf: joi.string().min(11).max(11).required(),
   birthday: joi.required(),
 });
