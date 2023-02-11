e0b0e22e3847715856043e8f3d76f04c140d03c4
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-11 09:51:29 -0300

    asda

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,57 +1,51 @@
 import { connectionDB } from "../database/db.js";
 
 export async function create(req, res) {
-  const { name, image, stockTotal, categoryId, pricePerDay } = res.locals.game;
+  
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
   try {
-    await connectionDB.query(
-      'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',
-      [name, image, stockTotal, categoryId, pricePerDay]
-    );
-
-    res.sendStatus(201);
-  } catch (err) {
-    res.status(500).send(err.message);
+      await connectionDB.query(
+          'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',
+          [name, image, stockTotal, categoryId, pricePerDay]);
+          
+        res.sendStatus(201);
+  } catch (error) {
+      res.status(500).send(error.message);
   }
 }
 
 export async function findAll(req, res) {
   const { name } = req.query;
+
   try {
-    let games;
-    if (name) {
+      
+      let allGames;
+
+      if(name) {
+          allGames = await connectionDB.query(
+              `SELECT games.*,
+              categories.name AS "categoryName" 
+              FROM games JOIN categories
+              ON games."categoryId" = categories.id
+              WHERE games.name ILIKE '%' || $1 || '%';`,[name]
+              );
+
+          return res.send(games.rows);
+          }
+
       games = await connectionDB.query(
-        `
-              SELECT games.*, categories.name AS "categoryName" 
-              FROM 
-                  games
-              JOIN
-                  categories
-              ON
-                  games."categoryId" = categories.id
-              WHERE
-                games.name ILIKE '%' || $1 || '%';
-          `,
-        [name]
-      );
-
-      return res.send(games.rows);
-    }
-
-    games = await connectionDB.query(
-      `
-          SELECT games.*, categories.name AS "categoryName" 
-          FROM 
-              games
-          JOIN
-              categories
-          ON
-              games."categoryId" = categories.id
-      `
-    );
-
-    res.send(games.rows);
-  } catch (err) {
-    res.status(500).send(err.message);
+              `
+              SELECT games.*,
+              categories.name AS "categoryName" 
+              FROM games JOIN categories
+              ON games."categoryId" = categories.id`
+            );
+        
+            res.send(games.rows);
+  } catch (error) {
+      res.status(500).send(error.message);
   }
+
+  res.status(200)
 }
--- src/models/Games.js
+++ src/models/Games.js
@@ -4,6 +4,5 @@ export const gameSchemma = joi.object({
   name: joi.string().min(2).required(),
   image: joi.string().min(2).required(),
   stockTotal: joi.number().positive().required(),
-  categoryId: joi.number().required(),
   pricePerDay: joi.number().positive().required(),
 });
