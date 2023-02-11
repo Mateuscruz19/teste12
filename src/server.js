789b46809393c8207c99c981fbb4c9c42d0405c5
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-11 06:42:39 -0300

    teste

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express, { application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import categoriesRoutes from "./routes/categories.routes.js";
+import gamesRoutes from "./routes/games.routes.js";
+import customersRoutes from "./routes/customers.routes.js";
+import rentalsRoutes from "./routes/rentals.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(categoriesRoutes);
+app.use(gamesRoutes);
+app.use(customersRoutes);
+app.use(rentalsRoutes);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
