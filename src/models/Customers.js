22ad21bd6efe81b174ce61ad1c40263dc82d9df7
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-11 08:09:51 -0300

    a

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const customerSchemma = joi.object({
   name: joi.string().min(2).required(),
-  phone: joioi.string().regex(/^[0-9]+$/).min(10).max(11).required(),
+  phone: joi.string().regex(/^[0-9]+$/).min(10).max(11).required(),
   cpf: joi.string().regex(/^[0-9]+$/).min(11).max(11).required(),
   birthday: joi.required(),
 });
